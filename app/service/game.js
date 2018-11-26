d51217cb9ee54220f6ce513cc436fd2d20990120
leopoldchen <placeintime@live.cn>
2018-11-26 12:26:05 +0800

    refactoring, move encode decode to game service

--- app/service/game.js
+++ app/service/game.js
@@ -1,11 +1,15 @@
 'use strict';
 
 const net = require('net');
+const ProtoBuf = require('protobufjs');
+const Base64 = require('js-base64').Base64;
 const Service = require('egg').Service;
 
-class GameService extends Service {
-  async sendMessage(message) {
+const Root = ProtoBuf.Root.fromJSON(require('../../proto/hall.json'));
+const CSMessageHeader = Root.lookupType('CSMessageHeader');
 
+class GameService extends Service {
+  async send(message) {
     const client = net.Socket();
     await client.connect({
       host: this.config.gameServer.host,
@@ -16,6 +20,52 @@ class GameService extends Service {
       await client.destroy();
       resolve(data);
     }));
+  }
+
+  async encode(msgHeader, reqBuf) {
+    const headerBuf = CSMessageHeader.encode(msgHeader).finish();
+    const headerLen = headerBuf.length + 4;
+    const totalLen = reqBuf.length + headerLen + 4;
+    const headerLenBuf = Buffer.allocUnsafe(4);
+    headerLenBuf.writeUInt32LE(headerLen);
+    const totalLenBuf = Buffer.allocUnsafe(4);
+    totalLenBuf.writeUInt32LE(totalLen);
+    const message = Buffer.concat([ totalLenBuf, headerLenBuf, headerBuf, reqBuf ], totalLen);
+    return message;
+  }
+
+  async decode(data) {
+    const totalLen = data.readUInt32LE(4, 0);
+    const headerLen = data.readUInt32LE(4, 4);
+    const headerBuf = data.slice(8, headerLen + 4);
+    const rspBuf = data.slice(4 + headerLen, totalLen);
+    return {
+      headerBuf,
+      rspBuf,
+    };
+  }
+
+  async sendMessage(msgHeader, reqBuf) {
+    const message = await this.service.game.encode(msgHeader, reqBuf);
+    console.log('send: ', message);
+    const data = await this.service.game.send(message);
+    console.log('return: ', data);
+    const {
+      headerBuf,
+      rspBuf,
+    } = await this.service.game.decode(data);
+    const rspHeader = CSMessageHeader.decode(headerBuf);
+    console.log('decoded rspHeader: ', rspHeader);
+    if (rspHeader.errcode) throw new Error('Hall Error: ', rspHeader.errcode);
+    const rspClassName = Base64.decode(rspHeader.msg_name.toString());
+    console.log('rspClassName: ', rspClassName);
+    const decoded = Root.lookupType(rspClassName).decode(rspBuf);
+    console.log('decoded: ', JSON.stringify(decoded));
+    return decoded;
+  }
+
+  async getProtoClass(name) {
+    return Root.lookupType(name);
   }
 }
 module.exports = GameService;
--- app/service/hall.js
+++ app/service/hall.js
@@ -1,50 +1,9 @@
 'use strict';
 
-const ProtoBuf = require('protobufjs');
 const Base64 = require('js-base64').Base64;
 const Service = require('egg').Service;
 
-const hallRoot = ProtoBuf.Root.fromJSON(require('../../proto/hall.json'));
-const CXIsLoginToHallReq = hallRoot.lookupType('CXIsLoginToHallReq');
-const CXIsLoginToHallRsp = hallRoot.lookupType('CXIsLoginToHallRsp');
-const CSMessageHeader = hallRoot.lookupType('CSMessageHeader');
-
 class HallService extends Service {
-  async encode(msgHeader, reqBuf) {
-    const headerBuf = CSMessageHeader.encode(msgHeader).finish();
-    const headerLen = headerBuf.length + 4;
-    const totalLen = reqBuf.length + headerLen + 4;
-    const headerLenBuf = Buffer.allocUnsafe(4);
-    headerLenBuf.writeUInt32LE(headerLen);
-    const totalLenBuf = Buffer.allocUnsafe(4);
-    totalLenBuf.writeUInt32LE(totalLen);
-    const message = Buffer.concat([ totalLenBuf, headerLenBuf, headerBuf, reqBuf ], totalLen);
-    return message;
-  }
-
-  async decode(data) {
-    const totalLen = data.readUInt32LE(4, 0);
-    const headerLen = data.readUInt32LE(4, 4);
-    const headerBuf = data.slice(8, headerLen + 4);
-    const rspBuf = data.slice(4 + headerLen, totalLen);
-    return {
-      headerBuf,
-      rspBuf,
-    };
-  }
-
-  async sendMessage(message) {
-    console.log('send: ', message);
-    const data = await this.service.game.sendMessage(message);
-    console.log('return: ', data);
-    const { headerBuf, rspBuf } = await this.service.hall.decode(data);
-    console.log('decoded headerBuf: ', headerBuf);
-    const msgHeader = CSMessageHeader.decode(headerBuf);
-    if (msgHeader.errcode) throw new Error('Hall Error: ', msgHeader.errcode);
-
-    return rspBuf;
-  }
-
   async isLoginToHall(options) {
     const req = {
       uid: options.uid,
@@ -52,17 +11,14 @@ class HallService extends Service {
       token: Base64.encode(options.token),
       is_need_user_info: options.is_need_user_info,
     };
-    const gatewaySession = Date.now();
+    const gatewaySession = `${Date.now()}${Math.ceil(Math.random() * 100)}`;
     const msgHeader = {
       msg_name: Base64.encode('CXIsLoginToHallReq'),
       gateway_session: gatewaySession,
     };
-
+    const CXIsLoginToHallReq = this.service.game.getProtoClass('CXIsLoginToHallReq');
     const reqBuf = CXIsLoginToHallReq.encode(req).finish();
-    const message = await this.service.hall.encode(msgHeader, reqBuf);
-
-    const rspBuf = await this.service.hall.sendMessage(message);
-    const decoded = CXIsLoginToHallRsp.decode(rspBuf);
+    const decoded = await this.service.game.sendMessage(msgHeader, reqBuf);
     return decoded;
   }
 }
